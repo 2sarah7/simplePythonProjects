1faf0a62550c231de55767f0b143b9331b7121c4
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2023-07-03 09:22:28 -0600

    improved game - now says if number is too high or low - doesn't entirely clear the input box though - need to fix that

--- numberGuess.py
+++ numberGuess.py
@@ -2,10 +2,10 @@
 import tkinter as tk
 import random
 
-#makes "num" into a number between 1-10
-
+#picks a random number
+x = 100
 num_wins = 0
-num = random.randint(1,10)
+num = random.randint(1,x)
 
 
 #this runs when the button is clicked
@@ -23,14 +23,20 @@ def get_num(wins,number):
         global num_wins
         num_wins += 1
         global num
-        num = random.randint(1,10)
+        num = random.randint(1,x)
         print(num)
         user_input.delete(1)
         num_of_wins.config(text=f'Number of wins: {num_wins}')
         win_label.config(text="win")
-    else:
-        win_label.config(text="Lose")      
+    elif user_input_int <= number:
+        win_label.config(text="Number is too small")
         user_input.delete(0)
+    elif user_input_int >= number:
+        win_label.config(text="Number is too big")
+        user_input.delete(0)
+#    else:
+#        win_label.config(text="Lose")      
+#        user_input.delete(0)
         
 
 
@@ -38,31 +44,31 @@ def get_num(wins,number):
 
 
 #makes window
-root = tk.Tk()
+gamewindow = tk.Tk()
 
-root.title("Guessing game")
-root.geometry("250x100")
+gamewindow.title("Guessing game")
+gamewindow.geometry("300x150")
 
-label = tk.Label(root, text="Guess a number between 1-10")
+label = tk.Label(gamewindow, text="Guess a number between 1-100")
 #calling the VAR "label" then ".pack()" will add it to the window
 label.pack()
 
 
-#allows the user to input there number
-user_input = tk.Entry(root)
+#allows the user to input their number
+user_input = tk.Entry(gamewindow)
 user_input.pack()
 
 #button to sumbit your response, "command" is what it runs when clicked
-button = tk.Button(root, text= "submit", command=lambda: get_num(num_wins,num))
+button = tk.Button(gamewindow, text= "submit", command=lambda: get_num(num_wins,num))
 button.pack()
 
-num_of_wins = tk.Label(root, text=f'number of wins:{num_wins}')
+num_of_wins = tk.Label(gamewindow, text=f'number of wins:{num_wins}')
 num_of_wins.pack()
 
 #empty text to change later
-win_label = tk.Label(root, text="")
+win_label = tk.Label(gamewindow, text="")
 win_label.pack()
 
 
 #loop
-root.mainloop() 
+gamewindow.mainloop() 
