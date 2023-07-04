1b092425e7becd512ba57eaea8647fe69eeb3507
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2023-07-03 18:28:05 -0600

    everything works except tryleft - only giving the person 3 tries total

---
+++ pinPasscode.py
@@ -0,0 +1,36 @@
+
+import tkinter as tk
+
+passpin = "1234"
+
+tryleft = 3
+
+def getpass(entered, pin, tries):
+    entered = userInput.get()
+    if tries > 0:
+        if entered == pin:
+            row.config(text=f"Password is correct, access granted. ")
+        else:
+            
+            tries -= 1
+
+            row.config(text=f"Password is incorrect, {tries} tries left")
+    else:
+        row.config(text=f"Too many failed atempts. Please try again later. ")
+
+window = tk.Tk()
+window.title("Enter password")
+window.geometry("400x200")
+heading = tk.Label(window, text="Please enter your password")
+heading.pack()
+
+userInput = tk.Entry(window)
+userInput.pack()
+
+button = tk.Button(window, text="Submit", command=lambda: getpass(userInput, passpin, tryleft) )
+button.pack()
+
+row = tk.Label(window, text=f"...")
+row.pack()
+
+window.mainloop()
