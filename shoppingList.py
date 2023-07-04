5039a8cf5f6194a0599f42aec5014125167bc677
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2023-07-04 08:42:35 -0600

    doesn't work entirely yet

---
+++ shoppingList.py
@@ -0,0 +1,17 @@
+shoppingList = []
+finished = False
+while finished == False:
+    aor = input("Would you like to add or remove items, or view the list? ")
+    if "ADD" in aor.upper():
+        item = input("What would you like to add? ")
+        shoppingList.append(item.upper())
+    elif "REMOVE" in aor.upper():
+        take = input("What would you like to remove? ")
+        if take.upper() in shoppingList:
+            shoppingList.remove(str(take.upper()))
+        else:
+            print(take, "is not in your shopping list. ")
+    elif "VIEW" in aor.upper():
+        print(shoppingList)
+    elif "FINISH" in aor.upper() or "DONE" in aor.upper():
+        finished = True
