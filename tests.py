d5caa0c12049698e5780c0b08539135c0365a5b8
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2023-07-22 21:32:50 -0600

    did stuff

---
+++ tests.py
@@ -0,0 +1,8 @@
+t = input("HI")
+u = int(t)
+#Run a loop to accept 't' inputs
+for i in range(u):     
+    #accept an integer N in each test case
+    N = int(input())         
+    #output the number mirror for each test case
+    print(N)           
