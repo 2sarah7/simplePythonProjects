7d733f825510f4b4165f30780e848549ce0cb45f
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2023-07-22 20:55:22 -0600

    web scraper - still doesnt work

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.chess.com/home"
-response = requests.get(url)
+#doesnt work
 
-soup = BeautifulSoup(response.content, "html.parser")
+URL = "http://www.values.com/inspirational-quotes"
+r = requests.get(URL)
 
+soup = BeautifulSoup(r.content, 'html5lib')
+
+quotes = []
+
+table = soup.find('div', attrs = {'id':'all_quotes'})
+
+for row in table.findAll('div', attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
+    #creates a dictionary
+    quote = {}
+    quote['theme'] = row.h5.text
+    quote['url'] = row.a['href']
+    quote['img'] = row.img['src']
+    quote['lines'] = row.img['alt'].split(" #")[0]
+    quote['author'] = row.img['alt'].split(" #")[1]
+    quotes.append(quote)
+
+filename = 'inspirational_quotes.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f,['theme','url','img','lines','author'])
+    w.writeheader()
+    for quote in quotes:
+        w.writerow(quote)
