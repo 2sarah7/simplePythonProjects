27aeaa35ddfd55ff793ef9ad4ddebd714f0ea94b
2sarah7 <138382677+2sarah7@users.noreply.github.com>
2023-07-27 12:39:59 -0600

    added file

---
+++ test.swift
@@ -0,0 +1,20 @@
+#Variables
+var str = "Hello, world"
+#Constants
+let number = 5
+#String Interpolation
+var str2 = "Your score was \(number)"
+print(str)
+print(str2)
+#Type inference
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let mybool: Bool = true
+
+#Arrays - in order you add them, numerical positions
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+let beatles = [john, paul, george, ringo]
